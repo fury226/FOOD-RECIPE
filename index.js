923539c3cf761724a1497ed241d98fc25e1e0283
fury226 <95533542+fury226@users.noreply.github.com>
2022-01-12 19:52:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+function recipe1(){
+    var window = document.querySelector(".text");
+   var text = " <h4>Beef steak with onion sauce</h4><br><h3>Ingredients</h3><p>- Steak: 350 grams - Onions: 5 pieces (large size / chopped) - Butter: 4 tablespoons- Thyme: a tablespoon (chopped) - Bay leaf: 1 leaf - Salt: half a teaspoon- Black pepper: a quarter of a teaspoon - Red grape juice: half a cup - Meat broth: a quarter of a cup-Cooking cream: a quarter cup)<h2>Steps</h2></p><br><p><sup><b>Step1</b></sup>Start preparing the onion mixture, by melting the butter in a large skillet over medium-low heat.Once the butter melts, add the onions, bay leaf and thyme and stir to coat the butter. Cook, stirring frequently, for 25-30 minutes, until onions are soft and brown.<br><sup><b>Step2</b></sup>Sweeten the pan with grape juice, stir and cook the juice for 3 minutes, then add the beef broth and cream, stir and turn off the heat.Remove the bay leaf, cover the pot and keep it warm.<br><sup><b>Step3</b></sup>Season the steaks with plenty of salt and pepper and place them on a grill over a medium-high heat.<br><sup><b>Step4</b></sup>Grill 6-7 minutes on each side, adding a few minutes to each side if you want a well-cooked steak.<br>.Remove the steak from the grill and let it rest covered with tin foil for 5 minutes.<br><sup><b>Step5</b></sup>Cut the steak into thick slices and serve it with the onion sauce.</p> "
+   window.innerHTML=text;
+   var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function recipe2(){
+    var window = document.querySelector(".text");
+    var text = "<h4>Beef stir fry with vegetables</h4><br><h3>Ingredients</h3><p>Meat: 500 grams (sliced)-Carrots: 1 piece (large size / sliced)-Broccoli: a cup (boiled)Red pepper: 1 piece-Corn: ½ cup (boiled)-Yellow pepper: 1 piece-Green onions: a bunch (chopped)Garlic: 1 tablespoon (crushed)-Vegetable oil: a tablespoon-Sesame oil: a teaspoon Ginger: 2 tablespoons (fresh)-Soy sauce: a quarter cup-Vinegar: a tablespoon-Sesame: half a cup (roasted)<h2>Steps</h2></p><br><p><sup><b>Step1</b></sup>In a frying pan, heat the oil, then add the meat and mix well.<br><sup><b>Step2</b></sup>Add garlic, ginger and onion and keep stirring.<br><sup><b>Step3</b></sup>Add pepper and soy sauce, then keep stirring well.<br><sup><b>Step4</b></sup> Before ripening, add sesame oil, vinegar and broccoli and let it simmer.<br><sup><b>Step5</b></sup>Garnish with sesame seeds and serve with white rice.</p>"
+    window.innerHTML=text;
+    var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function recipe3(){
+    var window = document.querySelector(".text");
+    var text = "<h4>Chicken Lasagna</h4><br><h3>Ingredients</h3><p>- Chicken breast: 3 breasts (boiled and chopped fillet) - Lasagna dough: 6 pieces - Chicken broth: 1 cube - Water: a quarter of a cup - Cream cheese: a package - Mozzarella cheese: two cups (grated) -Ready-made pasta sauce: 1 bottle<h2>Steps</h2></p><br><p><sup><b>Step1</b></sup>In a frying pan, heat the oil, then add the meat and mix well.<br><sup><b>Step2</b></sup>Preheat the oven to 175 degrees and dissolve the chicken broth cube in hot water in a bowl.<br><sup><b>Step3</b></sup>Add the cream cheese and 1 cup of shredded mozzarella cheese, mix, then spread 1/3 of the spaghetti sauce in a 9-inch baking dish.<br><sup><b>Step4</b></sup>Put the minced chicken, half of the cream cheese mixture and 3 pieces of lasagna, then repeat the ball to make a second layer of lasagna, chicken and sauce.<br><sup><b>Step5</b></sup>Spread the remaining cup of mozzarella cheese on top and put the dish in the oven for 45 minutes until the cheese melts and its feet are hot.</p>" 
+    window.innerHTML=text;
+    var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function recipe4(){
+    var window = document.querySelector(".text");
+    var text = "<h4>Margherita pizza with sauce and cheese</h4><br><h3>Ingredients</h3><p>For the dough:Flour: 3 and a half cups, Salt: a teaspoon, Water: a cup, Instant yeast: a tablespoon, Sugar: a teaspoonFor filling:Pizza sauce: 2 cups (ready)- Mozzarella cheese: 2 cups (grated)- Olive oil: 2 tablespoons<h2>Steps</h2></p><br><p><sup><b>Step1</b></sup>Mix the ingredients for the dough until a soft and cohesive dough is formed and leave it for 60 minutes to ferment.<br><sup><b>Step2</b></sup>Spread the dough in an oiled tray.<br><sup><b>Step3</b></sup>Distribute the prepared pizza sauce and olive oil and brush the dough well with it.<br><sup><b>Step4</b></sup>Spread mozzarella cheese over the dough.<br><sup><b>Step5</b></sup>Garnish with sesame seeds and serve with white rice.</p>"
+    window.innerHTML=text;
+    var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function recipe5(){
+    var window = document.querySelector(".text");
+    var text = "<h4>Grilled meatballs with parmesan cheese</h4><br><h3>Ingredients</h3><p>Minced meat: 500 grams- Salt: half a teaspoon- Onion: 1 piece (small size / grated) -Garlic powder: half a teaspoon Italian herbs: one and a half teaspoons (dried) -- Black pepper: a quarter of a teaspoon- Milk: a third of a cup- Parmesan cheese: a quarter of a cup (grated)-Rusks: half a cup (crushed cakes)</p><br><p><sup><b>Step1</b></sup>1. Preheat the oven to 200 degrees in advance and mix the minced meat with salt, black pepper, garlic powder, Italian herbs and finely grated onion.<br><sup><b>Step2</b></sup>Add milk, cheese and rusk and stir well until it becomes like dough.<br><sup><b>Step3</b></sup>Form medium-sized balls of meat and put them in the oven dish.<br><sup><b>Step4</b></sup>Put the dish in the oven for 25 minutes, until the meat is cooked and browned then Serve hot with salad or potatoes, as desired.<br></p>"
+    window.innerHTML=text;
+    var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function recipe6(){
+    var window = document.querySelector(".text");
+    var text = "<h4>Coleslaw with pineapple salad</h4><br><h3>Ingredients</h3><p>Cabbage: 1 piece (medium size, chopped small)-Carrot: 1 piece (grated)- Pineapple: can- Mayonnaise: a cup-Salt: a teaspoon</p><br><p><sup><b>Step1</b></sup>Cut the pineapple into medium-sized pieces, as well as the cabbage and carrots.<br><sup><b>Step2</b></sup>Mix the cabbage, carrots, pineapple, mayonnaise and salt in an appropriate size bowl.<br><sup><b>Step3</b></sup>Put the coleslaw salad in the refrigerator for two hours before serving.<br><sup><b>Step4</b></sup>Garnish with pineapple slices and serve.</p>"
+    window.innerHTML=text;
+    var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function recipe7(){
+    var window = document.querySelector(".text");
+    var text = "<h4>Cake</h4><br><h3>Ingredients</h3><p>Eggs: 3 grains-Milk: a cup- Vanilla: a tablespoon and a half- Baking powder: a teaspoon-Vegetable oil: a quarter of a cup-- Sugar: 2 cups-Flour: 2 cups</p><br><p><sup><b>Step1</b></sup>Bring a bowl and put the eggs in it.Put the sugar and vanilla on top of the eggs, then stir well. Put milk and oil on top and stir.<br><sup><b>Step2</b></sup>Put the flour and baking powder on top and stir well until the mixture becomes homogeneous<br><sup><b>Step3</b></sup>Put it in the oven and the oven must be hot before placing the mold.<br><sup><b>Step4</b></sup>Remove the pan from the oven after 45 minutes and insert a matchstick in the middle to make sure the cake is even.</p>"
+    window.innerHTML=text;
+    var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function recipe8(){
+    var window = document.querySelector(".text");
+    var text = "<h4>Lemon juice with mint and green apple</h4><br><h3>Ingredients</h3><p>- Lemon juice: half a cup-- Sugar: 3 tablespoons-- Green apple: 1 piece (peeled and chopped)-- Mint: a quarter cup (fresh)-- Ice: 4 pieces</p><br><p><sup><b>Step1</b></supPut lemon juice, sugar, apple, mint and ice in a blender><br><sup><b>Step2</b></sup>Beat the mixture for 5 minutes until the ingredients are combined.<br><sup><b>Step3</b></sup>Pour the mixture into cups and serve.</p>"
+    window.innerHTML=text;
+    var window1 = document.querySelector(".window");
+    window1.style.display='block';
+}
+function exit(){
+    var window1 = document.querySelector(".window");
+    window1.style.display="none";
+}
+function sub(){
+    var sub = document.querySelector(".sub").value;
+     alert("your order "+sub+" has been sent successfully")
+}
